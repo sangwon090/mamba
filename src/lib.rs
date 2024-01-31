63210f454bf93c966c7293fb95da59f081330898
Sangwon Ryu <me@sryu.dev>
2024-02-01 07:50:08 +0900

    Add support for wasm

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,22 @@
+use parser::ast::{AbstractSyntaxTree, Expression};
+use wasm_bindgen::prelude::*;
+
+use serde::{Serialize, Deserialize};
+use serde_json::{Result, Value};
+
 pub mod irgen;
 pub mod lexer;
 pub mod parser;
 pub mod error;
 pub mod types;
+
+use lexer::Lexer;
+use parser::Parser;
+
+#[wasm_bindgen]
+pub fn lex(string: String) -> String {
+    let mut lexer: Lexer = Lexer::new(string);
+    let tokens = lexer.get_tokens().unwrap();
+
+    serde_json::to_string(&tokens).unwrap()
+}
