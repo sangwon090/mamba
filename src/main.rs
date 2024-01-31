21a28421384cf936117b3ddfd4db696df889c82d
Sangwon Ryu <me@sryu.dev>
2024-02-01 07:50:26 +0900

    Print elapsed time

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io::{self, Write, BufRead};
 use std::fs;
 use std::env;
 use std::process::{Command, Stdio};
+use std::time::{Duration, Instant};
 
 use mamba::lexer::Lexer;
 use mamba::parser::Parser;
@@ -21,10 +22,10 @@ fn main() {
             let mut line = String::new();
             io::stdin().lock().read_line(&mut line).unwrap();
 
-            let mut lexer = Lexer::new(line);
+            let mut lexer: Lexer = Lexer::new(line);
             let tokens = lexer.get_tokens().unwrap();
 
-            let mut parser = Parser::new(tokens);
+            let mut parser: Parser = Parser::new(tokens);
             let ast = parser.parse_all();
 
             let mut irgen = IRGen::new(ast);
@@ -35,17 +36,23 @@ fn main() {
         let source: String = fs::read_to_string(file).unwrap();
 
         let mut lexer = Lexer::new(source);
+        let lexer_start = Instant::now();
         let tokens = lexer.get_tokens().unwrap();
+        println!(" lexer: {:?}", lexer_start.elapsed());
 
         let mut parser = Parser::new(tokens);
+        let parser_start = Instant::now();
         let ast = parser.parse_all();
+        println!("parser: {:?}", parser_start.elapsed());
 
         for statement in &ast.statements {
             println!("{}", statement.to_string());
         }
 
         let mut irgen = IRGen::new(ast);
+        let irgen_start = Instant::now();
         let ir = irgen.generate_ir().unwrap();
+        println!(" irgen: {:?}", irgen_start.elapsed());
         println!("===== Generated IR =====\n{}\n", ir);
 
         let mut llc = Command::new("llc")
