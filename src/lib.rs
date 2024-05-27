9f1b30a719a40fbb459485f0ec8e8f82c0ec48a1
Sangwon Ryu <me@sryu.dev>
2024-05-27 17:19:58 +0900

    Update the lexer output format

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
 pub mod irgen;
@@ -6,12 +7,49 @@ pub mod parser;
 pub mod error;
 pub mod types;
 
-use lexer::Lexer;
+use lexer::{Lexer, Token};
+
+#[derive(Serialize, Deserialize)]
+pub struct TokenOutput {
+    pub tokens: Vec<Token>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum MambaOutput {
+    Tokens(TokenOutput),
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum MambaProblem {
+    Error(String),
+    Warning(String),
+    Info(String),
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct MambaResult {
+    pub output: Option<MambaOutput>,
+    pub problems: Option<Vec<MambaProblem>>,
+}
 
 #[wasm_bindgen]
 pub fn lex(string: String) -> String {
     let mut lexer: Lexer = Lexer::new(string);
-    let tokens = lexer.get_tokens().unwrap();
 
-    serde_json::to_string(&tokens).unwrap()
+    let result = match lexer.get_tokens() {
+        Ok(tokens) => {
+            MambaResult {
+                output: Some(MambaOutput::Tokens(TokenOutput { tokens })),
+                problems: None,
+            }
+        },
+        Err(error) => {
+            MambaResult {
+                output: None,
+                problems: Some(vec![MambaProblem::Error(error.0)]),
+            }
+        }
+    };
+
+    serde_json::to_string(&result).unwrap()
 }
