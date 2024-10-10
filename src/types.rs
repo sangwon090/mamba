fcaa280f4d44dab5541839b195bad3c0d6dad720
Sangwon Ryu <me@sryu.dev>
2024-10-10 10:02:16 +0000

    Categorize data types for comparison and casting

--- src/types.rs
+++ src/types.rs
@@ -1,41 +1,49 @@
 use crate::lexer::Keyword;
 
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 #[allow(non_camel_case_types)]
-#[derive(Debug, PartialEq, Eq, Clone)]
 pub enum DataType {
-    // Boolean type
+    void,
     bool,
+    str,
 
-    // Signed integer types
+    SignedInteger(SignedInteger),
+    UnsignedInteger(UnsignedInteger),
+    FloatingPoint(FloatingPoint),
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[allow(non_camel_case_types)]
+pub enum SignedInteger {
     i8,
     i16,
     i32,
     i64,
     i128,
+}
 
-    // Unsigned integer types
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[allow(non_camel_case_types)]
+pub enum UnsignedInteger {
     u8,
     u16,
     u32,
     u64,
     u128,
+}
 
-    // Floating-point types
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[allow(non_camel_case_types)]
+pub enum FloatingPoint {
     f32,
     f64,
     f128,
-
-    // String types
-    str,
-
-    // Void
-    void,
 }
 
 impl From<Keyword> for DataType {
     fn from(value: Keyword) -> Self {
         match value {
-            Keyword::Int => DataType::i64,
+            Keyword::Int => DataType::SignedInteger(SignedInteger::i64),
             Keyword::Str => DataType::str,
             Keyword::Void => DataType::void,
             _ => panic!("Cannot convert {:?} into DataType!", value),
@@ -46,53 +54,28 @@ impl From<Keyword> for DataType {
 impl DataType {
     pub fn to_mnemonic(&self) -> &'static str {
         match &self {
-            DataType::bool  => "i1",
-
-            DataType::i8 => "i8",
-            DataType::i16 => "i16",
-            DataType::i32 => "i32",
-            DataType::i64 => "i64",
-            DataType::i128 => "i128",
-            DataType::u8 => "u8",
-            DataType::u16 => "u16",
-            DataType::u32 => "u32",
-            DataType::u64 => "u64",
-            DataType::u128 => "u128",
-
-            DataType::f32 => "float",
-            DataType::f64 => "double",
-            DataType::f128 => "fp128",
-
-            DataType::str => "i8*",
-
             DataType::void => "void",
+            DataType::bool  => "i1",
+            DataType::str => "i8*",
+            DataType::SignedInteger(dtype) => match dtype {
+                SignedInteger::i8 => "i8",
+                SignedInteger::i16 => "i16",
+                SignedInteger::i32 => "i32",
+                SignedInteger::i64 => "i64",
+                SignedInteger::i128 => "i128",
+            },
+            DataType::UnsignedInteger(dtype) => match dtype {
+                UnsignedInteger::u8 => "u8",
+                UnsignedInteger::u16 => "u16",
+                UnsignedInteger::u32 => "u32",
+                UnsignedInteger::u64 => "u64",
+                UnsignedInteger::u128 => "u128",
+            },
+            DataType::FloatingPoint(dtype) => match dtype {
+                FloatingPoint::f32 => "float",
+                FloatingPoint::f64 => "double",
+                FloatingPoint::f128 => "fp128",
+            },
         }
     }
-
-    pub fn is_bool(&self) -> bool {
-        if *self == DataType::bool {
-            true
-        } else {
-            false
-        }
-    }
-
-    pub fn is_integer(&self) -> bool {
-        match self {
-            DataType::bool | 
-            DataType::i8 | DataType::i16 | DataType::i32 | DataType::i64 |
-            DataType::u8 | DataType::u16 | DataType::u32 | DataType::u64 => true,
-            _ => false,
-        }
-    }
-
-    pub fn is_void(&self) -> bool {
-        if *self == DataType::void {
-            true
-        } else {
-            false
-        }
-    }
-
-
 }
